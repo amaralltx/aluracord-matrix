c280ab67f126e46b56b32edc063c868050d89813
Lucas Amaral <amarall.tx@gmail.com>
2022-01-25 23:17:50 -0300

    segunda aula

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import React from "react";
+import { useRouter } from 'next/router'
 import appConfig from "../config.json";
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -53,19 +24,22 @@ function Titulo(props) {
 }
 
 export default function PaginaInicial() {
-  const username = "amaralltx";
-
+  // const username = "rubenszinho";
+  const [username, setUsername]= React.useState("");
+  const roteamento = useRouter();
   return (
     <>
-      <GlobalStyle />
+      
       <Box
         styleSheet={{
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          backgroundColor: appConfig.theme.colors.primary[500],
+          backgroundColor: appConfig.theme.colors.neutrals[600],
+          /*
           backgroundImage:
-            "url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)",
+            "url(https://mcdn.wallpapersafari.com/medium/84/26/rTlqVG.png)
+          */
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
           backgroundBlendMode: "multiply",
@@ -92,6 +66,12 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              console.log("Submeteu")
+              roteamento.push('/chat')
+              //window.location.href = '/chat';
+            }}
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -112,8 +92,28 @@ export default function PaginaInicial() {
             >
               {appConfig.name}
             </Text>
-
+            
+            {/*
+            <input 
+              type="text"
+              value={username}
+              onChange={function (e) {
+                //onde está o valor digitado
+                const value = e.target.value;
+                //chamada para alterar o valor
+                setUsername(value);
+              }}>
+            </input>
+            */}    
+             
             <TextField
+              value={username}
+              onChange={function (e) {
+                //onde está o valor digitado
+                const value = e.target.value;
+                //chamada para alterar o valor
+                setUsername(value);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -124,6 +124,7 @@ export default function PaginaInicial() {
                 },
               }}
             />
+          
             <Button
               type="submit"
               label="Entrar"
